4c53a962eae3cef6a7827f4195989cca58dfd464
Will Thomas <30wthomas@gmail.com>
2025-07-31 11:30:42 -0500

    Some logging improvements for restricting when logging occurs

--- theories/Logging.v
+++ theories/Logging.v
@@ -8,6 +8,40 @@ Inductive LogLevel : Type :=
 | Error
 | Critical.
 
+Definition log_le (l1 l2 : LogLevel) : bool :=
+  match l1, l2 with
+  | Debug, _ => true
+  | _, Debug => false
+  | Info, _ => true
+  | _, Info => false
+  | Warning, _ => true
+  | _, Warning => false
+  | Error, _ => true
+  | _, Error => false
+  | Critical, Critical => true
+  end.
+
+Lemma log_le_refl : forall l, log_le l l = true.
+Proof.
+  destruct l; simpl; auto.
+Qed.
+
+Lemma log_le_antisymm : forall l1 l2,
+  log_le l1 l2 = true -> log_le l2 l1 = true -> l1 = l2.
+Proof.
+  destruct l1, l2; simpl; intros; try congruence.
+Qed.
+
+Lemma log_le_trans : forall l1 l2 l3,
+  log_le l1 l2 = true -> 
+  log_le l2 l3 = true -> 
+  log_le l1 l3 = true.
+Proof.
+  destruct l1, l2, l3; simpl; eauto.
+Qed.
+
+Global Opaque log_le.
+
 Definition log_level_str (level : LogLevel) : string :=
   match level with
   | Debug => "[DEBUG] "
@@ -18,7 +52,7 @@ Definition log_level_str (level : LogLevel) : string :=
   end.
 Global Opaque log_level_str.
 
-Definition log (level : LogLevel) (message : string) : unit :=
+Definition direct_log (level : LogLevel) (message : string) : unit :=
   match level with
   | Debug => TextIO.printLn_err (String.append (log_level_str Debug) message)
   | Info => TextIO.printLn_err (String.append (log_level_str Info) message)
@@ -26,15 +60,22 @@ Definition log (level : LogLevel) (message : string) : unit :=
   | Error => TextIO.printLn_err (String.append (log_level_str Error) message)
   | Critical => TextIO.printLn_err (String.append (log_level_str Critical) message)
   end.
+Global Opaque direct_log.
+
+Definition log (print_level : LogLevel) (level : LogLevel) 
+    (message : string) : unit :=
+  if log_le print_level level 
+  then direct_log level message
+  else tt.
 Global Opaque log.
 
-Definition log_debug (message : string) : unit :=
-  log Debug message.
-Definition log_info (message : string) : unit :=
-  log Info message.
-Definition log_warning (message : string) : unit :=
-  log Warning message.
-Definition log_error (message : string) : unit :=
-  log Error message.
-Definition log_critical (message : string) : unit :=
-  log Critical message.
+Definition direct_log_debug (message : string) : unit :=
+  direct_log Debug message.
+Definition direct_log_info (message : string) : unit :=
+  direct_log Info message.
+Definition direct_log_warning (message : string) : unit :=
+  direct_log Warning message.
+Definition direct_log_error (message : string) : unit :=
+  direct_log Error message.
+Definition direct_log_critical (message : string) : unit :=
+  direct_log Critical message.
